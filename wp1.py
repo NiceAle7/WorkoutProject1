05642e7247a0d9d73ae46044fc0b2878e138c3fd
Alejandro Olivares-Lopez <OLIVARA5@uci.edu>
2024-01-28 16:08:53 -0800

    Wrote a function called cipher that takes a file and reads it and stores it into a string variable called contents. Then contents is reverse and written into a new file.

--- wp1.py
+++ wp1.py
@@ -1,5 +1,5 @@
 import os
-def cipher(file, file2 = None):
+def cipher(file, file2):
     if os.path.exists(file):
         with open(f'{file}', 'r') as file1:
             contents = file1.read().strip()
@@ -9,8 +9,8 @@ def cipher(file, file2 = None):
 
 
 def main():
-    user_files = input("Enter a file to cipher")
-    cipher(user_files)
+    user_files = input("Enter a file to cipher and a new file: ").split()
+    cipher(user_files[0], user_files[1])
 
 if __name__ == "__main__":
     main()
